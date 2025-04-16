1bc34c3d8b3ade9ffa3298184d9a5a36c9cca80f
michaelabuchanan <michaela.buchanan@markiisys.com>
2025-04-16 22:01:49 +0000

    no more apex

--- llm-train.sh
+++ llm-train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pip install --no-cache-dir --upgrade transformers accelerate bitsandbytes peft datasets einops torch
+pip install transformers accelerate bitsandbytes peft datasets einops torch
+pip uninstall -y apex
 python /workspace/gitdata/runai/runai-llm-train.py
