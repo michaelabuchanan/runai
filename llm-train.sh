c7c29808fd81b18c5a8f146736652d140b197946
michaelabuchanan <michaela.buchanan@markiisys.com>
2025-04-16 21:29:23 +0000

    upgrade pip

--- llm-train.sh
+++ llm-train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python -m pip install --upgrade pip
 pip install --no-cache-dir --upgrade transformers accelerate bitsandbytes==0.45.5 peft datasets einops torch==2.6.0+cu124
 python /workspace/gitdata/runai/runai-llm-train.py
