9c01ab239837bffd9dfbfabfff952f1b38889a0e
michaelabuchanan <michaela.buchanan@markiisys.com>
2025-04-16 21:45:09 +0000

    changing containers

--- llm-train.sh
+++ llm-train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install --no-cache-dir --upgrade transformers accelerate bitsandbytes peft datasets einops torch==1.7.0
+pip install --no-cache-dir --upgrade transformers accelerate bitsandbytes peft datasets einops torch
 python /workspace/gitdata/runai/runai-llm-train.py
