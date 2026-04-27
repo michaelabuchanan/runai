33e8993fd6f1efeebc2c44cbe1a48ad6e120a436
Michaela Buchanan <michaela.buchanan@markiiisys.com>
2026-04-27 10:35:44 -0700

    Update llm-train.sh
    comment out uninstall apex

--- llm-train.sh
+++ llm-train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip install transformers accelerate bitsandbytes peft datasets einops torch torchao
-pip uninstall -y apex
+# pip uninstall -y apex
 python /workspace/runai/runai-llm-train.py --lr=$1
